9a607f32e244aec55fcf85ba05cc1344be607a62
Felix <fdelapazf@gmail.com>
2026-02-18 23:59:30 +0100

    fix black formatting

--- src/app.py
+++ src/app.py
@@ -25,11 +25,9 @@ def favorite_colors() -> List[Dict]:
 
     return results
 
-
 @app.route("/")
 def index() -> str:
     return json.dumps({"favorite_colors": favorite_colors()})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
