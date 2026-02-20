244022b16adf3a0f1a6089aa90edc70d320ea53b
Felix <fdelapazf@gmail.com>
2026-02-20 18:50:31 +0100

    a ver si fnciona

--- src/app.py
+++ src/app.py
@@ -4,11 +4,9 @@ import psycopg2
 import json
 import os
 
-
 app = Flask(__name__)
 
-# comment
-
+# ~comment
 
 def favorite_colors() -> List[Dict]:
     config = {
@@ -27,11 +25,9 @@ def favorite_colors() -> List[Dict]:
 
     return results
 
-
 @app.route("/")
 def index() -> str:
     return json.dumps({"favorite_colors": favorite_colors()})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
